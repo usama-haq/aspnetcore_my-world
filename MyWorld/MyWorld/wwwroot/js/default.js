70df29e2a235ada663b41c03ffe447f7d8dcc0f7
Usama Haq <usamahaq@outlook.com>
2017-08-17 19:39:11 +0500

    4.4 JavaScript Events

--- MyWorld/MyWorld/wwwroot/js/default.js
+++ MyWorld/MyWorld/wwwroot/js/default.js
@@ -1,2 +1,12 @@
 ﻿var e = document.getElementById("username");
 e.innerHTML = "Sammy";
+
+
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+    main.style.backgroundColor = "#888";
+};
+
+main.onmouseleave = function () {
+    main.style.backgroundColor = "";
+};
