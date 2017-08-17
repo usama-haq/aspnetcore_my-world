df36b83e5370c8d0c7933baf6e5090c56baa0c0d
Usama Haq <usamahaq@outlook.com>
2017-08-17 20:06:59 +0500

    4.7 Introducing jQuery

--- MyWorld/MyWorld/wwwroot/js/default.js
+++ MyWorld/MyWorld/wwwroot/js/default.js
@@ -1,13 +1,20 @@
 ﻿(function () {
-    var e = document.getElementById("username");
-    e.innerHTML = "Sammy";
+    var e = jQuery("#username");
+    e.text("Sammy");
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function () {
-        main.style.backgroundColor = "#888";
-    };
+    var main = jQuery("#main");
 
-    main.onmouseleave = function () {
-        main.style.backgroundColor = "";
-    };
+    main.on("mouseenter", function () {
+        main.style = "background-color: #888;";
+    });
+
+    main.on("mouseleave", function () {
+        main.style = "";
+    });
+
+    var menuitems = jQuery("ul.menu li a");
+    menuitems.on("click", function () {
+        var me = jQuery(this);
+        alert(me.text());
+    });
 })();
