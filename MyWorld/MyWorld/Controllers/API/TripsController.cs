1c5208756dabfe58878833288b98fc9c082c6266
Usama Haq <usamahaq@outlook.com>
2017-08-20 03:14:14 +0500

    8.2 Create an API Controller

---
+++ MyWorld/MyWorld/Controllers/API/TripsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyWorld.Models;
+
+namespace MyWorld.Controllers.API
+{
+    public class TripsController : Controller
+    {
+        [HttpGet("api/trips")]
+        public IActionResult Get()
+        {
+            return Ok(new Trip() { Name = "My Trip" });
+        }
+    }
+}
