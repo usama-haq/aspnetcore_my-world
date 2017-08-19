2a1d1f2e0e47435a98cc209411873de1c783f0ba
Usama Haq <usamahaq@outlook.com>
2017-08-19 05:42:57 +0500

    5.3 Enabling MVC

--- MyWorld/MyWorld/Startup.cs
+++ MyWorld/MyWorld/Startup.cs
@@ -11,6 +11,7 @@ namespace MyWorld
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -20,6 +21,14 @@ namespace MyWorld
             loggerFactory.AddConsole();
 
             app.UseStaticFiles();   // Server static files from wwwroot directory
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" }
+                    );
+            });
         }
     }
 }
