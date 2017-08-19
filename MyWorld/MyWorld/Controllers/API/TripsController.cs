68aa7564fa3daadad4937456a701d106aae4a2d4
Usama Haq <usamahaq@outlook.com>
2017-08-20 03:57:31 +0500

    8.7 Using Logging

--- MyWorld/MyWorld/Controllers/API/TripsController.cs
+++ MyWorld/MyWorld/Controllers/API/TripsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using MyWorld.Models;
 using MyWorld.ViewModels;
 using System;
@@ -11,10 +12,12 @@ namespace MyWorld.Controllers.API
     public class TripsController : Controller
     {
         private IWorldRepository _repo;
+        private ILogger<TripsController> _logger;
 
-        public TripsController(IWorldRepository repo)
+        public TripsController(IWorldRepository repo, ILogger<TripsController> logger)
         {
             _repo = repo;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -27,10 +30,8 @@ namespace MyWorld.Controllers.API
             }
             catch (Exception ex)
             {
-                // TODO: Log the exception messgage.
-
+                _logger.LogError($"-- Exception in Get() of TripsController: {ex}");
                 return BadRequest("> Error Occured.");
-                
             }
         }
 
