c24e88a80aa8c03a71d76bcb9d4c5c9d1cf42cd2
Usama Haq <usamahaq@outlook.com>
2017-08-20 02:21:57 +0500

    7.5 Using Migrations

---
+++ MyWorld/MyWorld/Migrations/20170819211911_Initial.Designer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MyWorld.Models;
+
+namespace MyWorld.Migrations
+{
+    [DbContext(typeof(WorldContext))]
+    [Migration("20170819211911_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("MyWorld.Models.Stop", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Arrival");
+
+                    b.Property<double>("Latitude");
+
+                    b.Property<double>("Longitude");
+
+                    b.Property<string>("Name");
+
+                    b.Property<int>("Order");
+
+                    b.Property<int?>("TripId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("TripId");
+
+                    b.ToTable("Stops");
+                });
+
+            modelBuilder.Entity("MyWorld.Models.Trip", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("DateCreated");
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("UserName");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Trips");
+                });
+
+            modelBuilder.Entity("MyWorld.Models.Stop", b =>
+                {
+                    b.HasOne("MyWorld.Models.Trip")
+                        .WithMany("Stops")
+                        .HasForeignKey("TripId");
+                });
+        }
+    }
+}
