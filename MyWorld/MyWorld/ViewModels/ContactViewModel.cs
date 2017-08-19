db45216870646a875a5d1b0d8651a7246cb27f5d
Usama Haq <usamahaq@outlook.com>
2017-08-19 06:29:26 +0500

    5.8 Using Validation

--- MyWorld/MyWorld/ViewModels/ContactViewModel.cs
+++ MyWorld/MyWorld/ViewModels/ContactViewModel.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MyWorld.ViewModels
 {
     public class ContactViewModel
     {
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(4096, MinimumLength = 10)]
         public string Message { get; set; }
     }
 }
