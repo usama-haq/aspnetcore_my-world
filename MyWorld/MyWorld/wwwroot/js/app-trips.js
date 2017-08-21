a7e972379ed44d27b5bbdbf0ad63ef2dc6c989af
Usama Haq <usamahaq@outlook.com>
2017-08-22 01:19:00 +0500

    11.2 Adding Routing

--- MyWorld/MyWorld/wwwroot/js/app-trips.js
+++ MyWorld/MyWorld/wwwroot/js/app-trips.js
@@ -3,5 +3,16 @@
     "use strict";
 
     // Creating the Module
-    angular.module("app-trips", ["simpleControls"]);
+    angular.module("app-trips", ["simpleControls", "ngRoute"])
+        .config(function ($routeProvider) {
+            $routeProvider.when("/", {
+                controller: "tripsController",
+                controllerAs: "vm",
+                templateUrl: "/views/tripsView.html"
+            });
+
+            $routeProvider.otherwise({
+                redirectTo: "/"
+            });
+        });
 })();
