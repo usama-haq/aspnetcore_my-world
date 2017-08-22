f917cbeec50b82439ebd796d6e216799d24551ae
Usama Haq <usamahaq@outlook.com>
2017-08-22 07:47:14 +0500

    12.5 Minifying AngularJS Code

--- MyWorld/MyWorld/gulpfile.js
+++ MyWorld/MyWorld/gulpfile.js
@@ -1,9 +1,11 @@
 ﻿/// <binding AfterBuild='minify' />
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var ngAnotate = require('gulp-ng-annotate');
 
 gulp.task('minify', function () {
     return gulp.src("wwwroot/js/*.js")
+        .pipe(ngAnotate())
         .pipe(uglify())
         .pipe(gulp.dest("wwwroot/lib/_app"));
 });
